a773685568b696677cfa8f38d987399d1af6d5eb
EDsnows <eduardo.castroneves19@gmail.com>
2023-12-30 16:27:21 -0300

    analisando o problema da API

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,19 @@
-const chaveDaAPI = "b68a014fd7a743bab3b05616232511"
-const botaoDeBusca = document.querySelector(".btn-busca")
+const chaveDaApi = "b68a014fd7a743bab3b05616232511";
+
+const botaoDeBusca = document.querySelector(".btn-busca");
 
 botaoDeBusca.addEventListener("click", async () => {
     const cidade = document.getElementById("input-busca").value;
 
-    if(!cidade) return;
+    if (!cidade) return;
 
     const dados = await buscarDadosDaCidade(cidade);
 
-    if(dados) preencherDadosNaTela(dados, cidade);
-})
+    if (dados) preencherDadosNaTela(dados, cidade);
+});
 
 async function buscarDadosDaCidade(cidade) {
-    const apiUrl = `https://api.weatherapi.com/v1/current.json?hey=${chaveDaAPI}&q=${cidade}&aqi=no&lang=pt`
+    const apiUrl = `https://api.weatherapi.com/v1/current.json?hey=${chaveDaApi}&q=${cidade}&aqi=no&lang=pt`
 
     const resposta = await fetch(apiUrl);
 
@@ -42,3 +43,54 @@ function preencherDadosNaTela(dados, cidade) {
 
     document.getElementById("icone-condicao").setAttribute("src", iconeCondicao)
 }
+
+
+
+
+// const chaveDaApi = "b68a014fd7a743bab3b05616232511";
+
+// const botaoDeBusca = document.querySelector(".btn-busca");
+
+// botaoDeBusca.addEventListener("click", async () => {
+//   const cidade = document.getElementById("input-busca").value;
+
+//   if (!cidade) return;
+
+//   const dados = await buscarDadosDaCidade(cidade);
+
+//   if (dados) preencherDadosNaTela(dados, cidade);
+// });
+
+// async function buscarDadosDaCidade(cidade) {
+//   const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${chaveDaApi}&q=${cidade}&aqi=no&lang=pt`;
+
+//   const resposta = await fetch(apiUrl);
+
+//   if (resposta.status !== 200) return;
+
+//   const dados = resposta.json();
+
+//   return dados;
+// }
+
+// function preencherDadosNaTela(dados, cidade) {
+//   const temperatura = dados.current.temp_c;
+//   const condicao = dados.current.condition.text;
+//   const humidade = dados.current.humidity;
+//   const velocidadeDoVento = dados.current.wind_kph;
+//   const iconeCondicao = dados.current.condition.icon;
+
+//   document.getElementById("cidade").textContent = cidade;
+
+//   document.getElementById("temperatura").textContent = `${temperatura} ºC`;
+
+//   document.getElementById("condicao").textContent = condicao;
+
+//   document.getElementById("humidade").textContent = `${humidade}%`;
+
+//   document.getElementById(
+//     "velocidade-do-vento"
+//   ).textContent = `${velocidadeDoVento} km/h`;
+
+//   document.getElementById("icone-condicao").setAttribute("src", iconeCondicao);
+// }
