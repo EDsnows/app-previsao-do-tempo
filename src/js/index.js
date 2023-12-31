3fb849e6853e5bf38e166ea3a11856c787f74127
EDsnows <eduardo.castroneves19@gmail.com>
2023-12-31 16:33:35 -0300

    funcionalidade com a API corrigida

--- src/js/index.js
+++ src/js/index.js
@@ -4,93 +4,42 @@ const botaoDeBusca = document.querySelector(".btn-busca");
 
 botaoDeBusca.addEventListener("click", async () => {
     const cidade = document.getElementById("input-busca").value;
-
+  
     if (!cidade) return;
-
+  
     const dados = await buscarDadosDaCidade(cidade);
-
+  
     if (dados) preencherDadosNaTela(dados, cidade);
 });
 
 async function buscarDadosDaCidade(cidade) {
-    const apiUrl = `https://api.weatherapi.com/v1/current.json?hey=${chaveDaApi}&q=${cidade}&aqi=no&lang=pt`
-
+    const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${chaveDaApi}&q=${cidade}&aqi=no&lang=pt`;
+  
     const resposta = await fetch(apiUrl);
-
-    if(resposta.status !== 200) return;
-
+  
+    if (resposta.status !== 200) return;
+  
     const dados = resposta.json();
-
+  
     return dados;
 }
 
 function preencherDadosNaTela(dados, cidade) {
-    const temperatura = dados.current.temp_c;
-    const condicao = dados.current.conditicion.text;
-    const humidade = dados.current.humidity;
-    const velocidadeDoVento = dados.current.wind_kph;
-    const iconeCondicao = dados.current.conditicion.icon;
-    
-    document.getElementById("cidade").textContent = cidade;
+  const temperatura = dados.current.temp_c;
+  const condicao = dados.current.condition.text;
+  const humidade = dados.current.humidity;
+  const velocidadeDoVento = dados.current.wind_kph;
+  const iconeCondicao = dados.current.condition.icon;
 
-    document.getElementById("temperatura").textContent = `${temperatura} ºC`;
+  document.getElementById("cidade").textContent = cidade;
 
-    document.getElementById("condicao").textContent = condicao;
+  document.getElementById("temperatura").textContent = `${temperatura} ºC`;
 
-    document.getElementById("humidade").textContent = `${humidade}%`;
+  document.getElementById("condicao").textContent = condicao;
 
-    document.getElementById("velocidade-do-vento").textContent = `${velocidadeDoVento} km/h`
+  document.getElementById("humidade").textContent = `${humidade}%`;
 
-    document.getElementById("icone-condicao").setAttribute("src", iconeCondicao)
+  document.getElementById("velocidade-do-vento").textContent = `${velocidadeDoVento} km/h`;
+
+  document.getElementById("icone-condicao").setAttribute("src", iconeCondicao);
 }
-
-
-
-
-// const chaveDaApi = "b68a014fd7a743bab3b05616232511";
-
-// const botaoDeBusca = document.querySelector(".btn-busca");
-
-// botaoDeBusca.addEventListener("click", async () => {
-//   const cidade = document.getElementById("input-busca").value;
-
-//   if (!cidade) return;
-
-//   const dados = await buscarDadosDaCidade(cidade);
-
-//   if (dados) preencherDadosNaTela(dados, cidade);
-// });
-
-// async function buscarDadosDaCidade(cidade) {
-//   const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${chaveDaApi}&q=${cidade}&aqi=no&lang=pt`;
-
-//   const resposta = await fetch(apiUrl);
-
-//   if (resposta.status !== 200) return;
-
-//   const dados = resposta.json();
-
-//   return dados;
-// }
-
-// function preencherDadosNaTela(dados, cidade) {
-//   const temperatura = dados.current.temp_c;
-//   const condicao = dados.current.condition.text;
-//   const humidade = dados.current.humidity;
-//   const velocidadeDoVento = dados.current.wind_kph;
-//   const iconeCondicao = dados.current.condition.icon;
-
-//   document.getElementById("cidade").textContent = cidade;
-
-//   document.getElementById("temperatura").textContent = `${temperatura} ºC`;
-
-//   document.getElementById("condicao").textContent = condicao;
-
-//   document.getElementById("humidade").textContent = `${humidade}%`;
-
-//   document.getElementById(
-//     "velocidade-do-vento"
-//   ).textContent = `${velocidadeDoVento} km/h`;
-
-//   document.getElementById("icone-condicao").setAttribute("src", iconeCondicao);
-// }
